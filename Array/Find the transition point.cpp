903edbb4ce30dd9b8e48fe209b6a5b6ee1156768
Sachin-05 <51017352+Sachin-05@users.noreply.github.com>
2023-07-07 17:59:43 +0530

    Update Find the transition point.cpp

--- Array/Find the transition point.cpp
+++ Array/Find the transition point.cpp
@@ -10,4 +10,25 @@ int transitionPoint(int arr[], int n) {
         }
     }
     return -1;
+
+    // // second approach :- 0 0 0 1 1 
+    // lets suppose maine mid find kar diya
+    // 0 aaya mtlb ans mai update kar do 
+    
+    int low = 0;
+    int high = n-1;
+    int ans = -1;
+    
+    while(low <= high){
+        int mid = (low + high) / 2;
+        
+        if(arr[mid] == 1){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    
+    return ans;
 }
